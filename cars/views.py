e49ec87ed43706e41b4360cf5880ac109edc5e4b
Vitor José <vitorjoseengel@hotmail.com>
2025-08-15 17:50:33 -0300

    Add Class Views

--- cars/views.py
+++ cars/views.py
@@ -1,27 +1,35 @@
 from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
+from django.views import View
 
 # Create your views here.
-def cars_view(request):
-    cars = Car.objects.all().order_by('model')
-    search = request.GET.get('search')
 
-    if search:
-        cars = cars.filter(model__icontains=search)
 
-    return render(
-        request,
-        'cars.html',
-        {'cars': cars }
-    )
+class CarsView(View):
 
-def new_car_view(request):
-    if request.method == 'POST' :
+    def get(self, request):
+        cars = Car.objects.all().order_by('model')
+        search = request.GET.get('search')
+
+        if search:
+            cars = cars.filter(model__icontains=search)
+
+        return render(
+            request,
+            'cars.html',
+            {'cars': cars }
+        )  
+
+class NewCarView(View):
+
+    def get(self, request):
+        new_car_form = CarModelForm()
+        return render(request, 'new_car.html', {'new_car_form' : new_car_form})
+    
+    def post(self, request):
         new_car_form = CarModelForm(request.POST, request.FILES)
         if new_car_form.is_valid():
             new_car_form.save()
             return redirect('cars_list')
-    else :
-        new_car_form = CarModelForm()
-    return render(request, 'new_car.html', {'new_car_form' : new_car_form})
+        return render(request, 'new_car.html', {'new_car_form' : new_car_form})

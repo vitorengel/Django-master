6926363c5eceaceded2c504911fe836154cd5897
Vitor José <vitorjoseengel@hotmail.com>
2025-08-20 18:36:59 -0300

    Implementado ClassBasedViews

--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
-from django.views import View
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 # Create your views here.
     
@@ -19,9 +17,24 @@ class CarsListView(ListView):
         return cars
     
     
-    
 class NewCarCreateView(CreateView):
     model = Car
     form_class = CarModelForm
     template_name = 'new_car.html'
     success_url = '/cars/'
+
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = 'car_detail.html'
+
+
+class CarUpdateView(UpdateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'car_update.html'
+    success_url = '/cars/'
+
+
+class CarDeleteView(DeleteView):
+    ...

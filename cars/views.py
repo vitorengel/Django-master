e61cb235a6bae93d6e99f281e032613d312a1d17
Vitor José <vitorjoseengel@hotmail.com>
2025-08-20 16:32:58 -0300

    Implementado ListView e CreateView

--- cars/views.py
+++ cars/views.py
@@ -2,34 +2,26 @@ from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.views import View
+from django.views.generic import ListView, CreateView
 
 # Create your views here.
-
-
-class CarsView(View):
-
-    def get(self, request):
-        cars = Car.objects.all().order_by('model')
-        search = request.GET.get('search')
-
-        if search:
-            cars = cars.filter(model__icontains=search)
-
-        return render(
-            request,
-            'cars.html',
-            {'cars': cars }
-        )  
-
-class NewCarView(View):
-
-    def get(self, request):
-        new_car_form = CarModelForm()
-        return render(request, 'new_car.html', {'new_car_form' : new_car_form})
     
-    def post(self, request):
-        new_car_form = CarModelForm(request.POST, request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars_list')
-        return render(request, 'new_car.html', {'new_car_form' : new_car_form})
+class CarsListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
+
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model')
+        search = self.request.GET.get('search')
+        if search:
+            cars = cars.filter(model_icontains=search)
+        return cars
+    
+    
+    
+class NewCarCreateView(CreateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'new_car.html'
+    success_url = '/cars/'

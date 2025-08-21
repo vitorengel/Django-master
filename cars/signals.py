692d97b5e7fad9d6f598f480bbefeac8f1ce2c78
Vitor José <vitorjoseengel@hotmail.com>
2025-08-21 17:16:39 -0300

    signals

--- cars/signals.py
+++ cars/signals.py
@@ -1,23 +1,31 @@
 from django.db.models.signals import pre_delete, pre_save, post_delete, post_save
+from django.db.models import Sum
 from django.dispatch import receiver
-from cars.models import Car
+from cars.models import Car, CarInventory
+
+
+def car_inventory_update():
+    cars_count = Car.objects.all().count()
+    cars_value = Car.objects.aggregate(
+        total_value=Sum('value')
+    )['total_value']
+    CarInventory.objects.create(
+        cars_count=cars_count,
+        cars_value=cars_value
+    )
 
 
 @receiver(pre_save, sender=Car)
 def car_pre_save(sender, instance, **kwargs):
-    ...
+    if not instance.bio:
+        instance.bio = 'Bio gerada automaticamente'
 
 
 @receiver(post_save, sender=Car)
-def car_pre_save(sender, instance, **kwargs):
-    ...
-
-
-@receiver(pre_delete, sender=Car)
-def car_pre_save(sender, instance, **kwargs):
-    ...
+def car_post_save(sender, instance, **kwargs):
+    car_inventory_update()
 
 
 @receiver(post_delete, sender=Car)
-def car_pre_save(sender, instance, **kwargs):
-    ...
+def car_post_delete(sender, instance, **kwargs):
+    car_inventory_update()

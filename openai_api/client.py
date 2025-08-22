0e71016a047fd7bcee3f0d3cae4c47e1f1bfb5b5
Vitor José <vitorjoseengel@hotmail.com>
2025-08-22 11:58:32 -0300

    Openai

---
+++ openai_api/client.py
@@ -0,0 +1,26 @@
+from openai import OpenAI
+
+
+client = OpenAI(
+    api_key='API_KEY'
+)
+
+
+def get_car_ai_bio(brand, model, year):
+    message = '''
+    Me mostre uma descrição de venda para o carro {} {} {} em apenas 250 caracteres. Fale coisas específicas sobre este carro.
+    '''
+
+    message = message.format(brand, model, year)
+    response = client.chat.completions.create(
+        messages=[
+            {
+                'role' : 'user',
+                'content' : message
+            }
+        ],
+        max_tokens=1000,
+        model='gpt-3.5-turbo',
+    )
+    
+    return response.choices[0].message.content
